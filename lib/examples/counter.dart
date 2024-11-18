1e8a9c9b94314cd41d6f4474d374125e0ad6ff64
barak.noam.haviv <barak.software.solutions@gmail.com>
2024-11-18 19:40:25 +0200

    create new counter App

--- lib/examples/counter.dart
+++ lib/examples/counter.dart
@@ -5,6 +5,6 @@ class CounterApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(home: Scaffold(body: Text('hello')));
   }
 }
